9a7da1d6938072ea7fd5acd166fce2e87ac992bf
Ein-Bar Surie <77734536+EB-303@users.noreply.github.com>
2022-07-20 12:05:09 +0300

    Create ScoreBoard.java

---
+++ ScoreBoard.java
@@ -0,0 +1,83 @@
+/*  *** WE HAVA A PROBLEM HOUSTON ! ***
+ * this program creates a ScoreBoard object and then prints the score of the game.
+ * but the program doesn't run and we don't have a way to get the score.
+ * 
+ *  -> we need you to fix this <-
+ * 
+ * complete the missing code in the ScoreBoard class.
+ * you can use the files from last lesson to help you.
+ * The lines are divided by difficulty level, challenge yourself .
+ * Good luck!
+*/
+
+
+public class ScoreBoard {
+    private static int player1 = 0;
+    // (?) What's missing ? complete the code here  ( Level 1)
+    // (?) What's missing ? complete the code here  ( Level 1)
+    private static int player4 = 0;
+
+    
+    public ScoreBoard(){
+        // Defaulte constractor
+    }
+
+
+// Getters & Setters for the private variables
+
+     // (?) What's missing ? complete the code here  ( Level 2)
+    public static void setPlayer1(){
+        player1++;
+    }
+
+    public static int getPlayer2(){
+        return player2;
+    }
+    public static void setPlayer2(){
+        player2++;
+    }
+   
+    public static int getPlayer3(){
+        return player3;
+    }
+    // (?) What's missing ? complete the code here  ( Level 3)
+
+    public static int getPlayer4(){
+        return player4;
+    }
+    public static void setPlayer4(){
+        player4++;
+    }
+  
+   //(?) What's missing ? complete the code here  ( *Chalenge* ) 
+   // Hint --> use the tamplate for functions from last lesson
+    
+        // this function will update the score of the player with the given number of player, this function without a return value
+        if(numOfPlayer == 1) 
+            setPlayer1();
+        else if(numOfPlayer == 2)
+            setPlayer2();
+        else if(numOfPlayer == 3) 
+            setPlayer3();
+        else 
+            setPlayer4();
+    }
+
+     
+    public static void showScore() {
+        // The function print the score  to the console 
+        System.out.println("Player 1: " + getPlayer1());
+        // What missing ? complete the code here  ( Level 2)
+        System.out.println("Player 3: " + getPlayer3());
+        System.out.println("Player 4: " + getPlayer4());
+    }
+
+    public static void main(String[] args) {
+        ScoreBoard.updateScore(2);
+        System.out.println(ScoreBoard.getPlayer2());
+
+        // Change the score of the player3 to 4 winnings, and print the score to the console
+        // Complete the code here  ( Level 3)
+
+    }
+}
